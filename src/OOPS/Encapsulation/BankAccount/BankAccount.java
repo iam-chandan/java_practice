cacd7215c5ce2a1c1133b0c10b5cb32522324133
PatroChandan <chandanpatro789@gmail.com>
2025-07-29 17:45:54 +0530

    bankAccount changes done

--- src/OOPS/Encapsulation/BankAccount/BankAccount.java
+++ src/OOPS/Encapsulation/BankAccount/BankAccount.java
@@ -3,8 +3,13 @@ package OOPS.Encapsulation.BankAccount;
 public class BankAccount {
     private long balance;
 
-    public void setData(long balance){
-        this.balance = balance;
+    public void setData(long amount){
+        if (amount > 0){
+            balance = amount;
+        }else {
+            System.out.println("Invalid Amount");
+            System.exit(0);
+        }
     }
     public long getData(){
         return balance;
