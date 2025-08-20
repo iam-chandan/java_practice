b2c70c901bbf29a59d3d94b16a94c8736dedb0fb
PatroChandan <chandanpatro789@gmail.com>
2025-08-20 22:03:00 +0530

    stall prigram update

--- src/multithreading/practice/Stall.java
+++ src/multithreading/practice/Stall.java
@@ -45,17 +45,21 @@ class Driver{
         Stall[] stalls =  new Stall[n];
         scn.nextLine();
         for (int i = 0; i < n; i++) {
-            System.out.println("enter stall name :");
-            String stallName = scn.nextLine();
-            System.out.println("enter stall details :");
-            String details = scn.nextLine();
-            System.out.println("enter stall area :");
-            double stallArea = scn.nextDouble();
-            scn.nextLine();
-            System.out.println("enter stall owner :");
-            String owner = scn.nextLine();
-            stalls[i] = new Stall(stallName,details,stallArea,owner);
+//            System.out.println("enter stall name :");
+//            String stallName = scn.nextLine();
+//            System.out.println("enter stall details :");
+//            String details = scn.nextLine();
+//            System.out.println("enter stall area :");
+//            double stallArea = scn.nextDouble();
+//            scn.nextLine();
+//            System.out.println("enter stall owner :");
+//            String owner = scn.nextLine();
+//            stalls[i] = new Stall(stallName,details,stallArea,owner);
 
+            System.out.println("All details about stall :");
+            String s = scn.nextLine();
+            String[] split = s.split(",");
+            stalls[i] =  new Stall(split[0],split[1],Integer.parseInt(split[2]),split[3]);
         }
 
         for (int i = 0; i < n; i++) {
@@ -75,3 +79,45 @@ class Driver{
         System.out.println(totalCost);
     }
 }
+
+/*
+3
+enter stall name :
+book stall
+enter stall details :
+stall for books
+enter stall area :
+25
+enter stall owner :
+ram
+enter stall name :
+food stall
+enter stall details :
+stall for food
+enter stall area :
+60
+enter stall owner :
+abhijit
+enter stall name :
+snack stall
+enter stall details :
+stall for snack
+enter stall area :
+30
+enter stall owner :
+chinmay
+17250.0
+ */
+
+/*
+    UDING .split(",") as we know split will return an array
+
+3
+All details about stall :
+book stall,stall for books,25,swarna
+All details about stall :
+food stall,stall for food,60,raj
+All details about stall :
+snack stall,stall for snack,30,abhijit
+17250.0
+ */
