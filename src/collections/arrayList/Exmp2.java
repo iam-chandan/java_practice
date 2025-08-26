fddf0180a2cd9cf236a3321e4f247d642e7e9079
PatroChandan <chandanpatro789@gmail.com>
2025-08-26 23:42:32 +0530

    Arraylist retrieve get(),subList(),indexOf(),lastIndexOf()

--- src/collections/arrayList/Exmp2.java
+++ src/collections/arrayList/Exmp2.java
@@ -1,7 +1,20 @@
 package collections.arrayList;
 
+import java.util.ArrayList;
+
 public class Exmp2 {
     public static void main(String[] args) {
-
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        System.out.println(al); // [10, 20, 30, 40, 50]
+        System.out.println(al.get(2)); // 30
+        System.out.println(al.subList(0,3)); // [10, 20, 30]
+        System.out.println(al.indexOf(30)); // 2
+        al.add(30);
+        System.out.println(al.lastIndexOf(30)); // 5
     }
 }
