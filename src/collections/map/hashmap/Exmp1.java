888cc3f41edbb888d9d80973de7fb7a642845cf1
PatroChandan <chandanpatro789@gmail.com>
2025-08-29 21:30:39 +0530

    map put()

---
+++ src/collections/map/hashmap/Exmp1.java
@@ -0,0 +1,14 @@
+package collections.map.hashmap;
+
+import java.util.HashMap;
+
+public class Exmp1 {
+    public static void main(String[] args) {
+        HashMap myDetails = new HashMap();
+        myDetails.put("firstName","chandan");
+        myDetails.put("lastName","patro");
+        myDetails.put("DOB","15-aug-1947");
+        myDetails.put("age",29);
+        System.out.println(myDetails);
+    }
+}
