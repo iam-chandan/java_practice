792ac9834447b33443dd5ea0b63f1cf0473fe93e
PatroChandan <chandanpatro789@gmail.com>
2025-11-29 17:52:56 +0530

    lambda and functional expression practice day4

---
+++ java_8_features/lambdaExpression/Employee.java
@@ -0,0 +1,4 @@
+package java_8_features.lambdaExpression;
+
+public class Employee {
+}
--- java_8_features/lambdaExpression/SortEmployee.java
+++ java_8_features/lambdaExpression/SortEmployee.java
@@ -1,4 +1,68 @@
 package java_8_features.lambdaExpression;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+
 public class SortEmployee {
+    public static void main(String[] args) {
+        List<Employee> list = Arrays.asList(new Employee(101,"abhijit",50000),
+                new Employee(102,"swarna",42000),
+                new Employee(103,"jintu",45000),
+                new Employee(104,"abhisekh",60000));
+        System.out.println("sorted according to id ::::::::");
+        list.stream().sorted((a,b)->a.getId()- b.getId()).forEach(System.out::println);
+
+        System.out.println("\nsorted according to name ::::::::");
+        list.stream().sorted((a,b)->a.getName().compareTo(b.getName())).forEach(System.out::println);
+
+        System.out.println("\nsorted according to salary ::::::::");
+        list.stream().sorted(Comparator.comparingDouble(Employee::getSalary)).forEach(System.out::println);
+    }
 }
